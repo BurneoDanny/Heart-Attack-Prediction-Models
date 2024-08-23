25ff23e6c41e9422dbc7dd2e4f81cf259d16f8aa
RicardoMolinaCoronel <ridumoli@espol.edu.ec>
2024-08-23 08:54:50 -0500

    csv

--- app.py
+++ app.py
@@ -101,6 +101,102 @@ def predict():
     except Exception as e:
         return jsonify({"error": f"Prediction failed: {str(e)}"}), 500
 
+def convert_types(data):
+    for key, value in data.items():
+        # Convertir a int si el valor puede ser convertido y el nombre del campo está en la lista de enteros
+        if key in ["age", "impluse", "pressureHight", "pressureLow", "gender"]:
+            try:
+                data[key] = int(value)
+            except ValueError:
+                pass  # Maneja el caso si el valor no puede ser convertido
+        # Convertir a float si el valor puede ser convertido y el nombre del campo está en la lista de flotantes
+        elif key in ["glucose", "kcm", "troponin"]:
+            try:
+                data[key] = float(value)
+            except ValueError:
+                pass  # Maneja el caso si el valor no puede ser convertido
+    return data
+
+@app.route('/batchpredict', methods=['POST'])
+def batchpredict():
+    data = request.json
+    print(data)
+    convert_types(data)
+    model_type = data.get('model')
+    print(data["age"])
+    print(model_type)
+    # Verificar que el modelo fue seleccionado correctamente
+    if model_type is None:
+        return jsonify({"error": "No model type provided"}), 400
+
+    try:
+        if model_type == 'mlp':
+            model = load_model('mlp_model.best')
+        elif model_type == 'logistic_regression':
+            model = load_model('logistic_regression_model')
+        elif model_type == 'decision_tree':
+            model = load_model('decision_tree_model')
+        elif model_type == 'random_forest':
+            model = load_model('random_forest_model')
+        elif model_type == 'svm':
+            model = load_model('svm_model')
+        elif model_type == 'gbm':
+            model = load_model('xgbm_model')
+        else:
+            return jsonify({"error": "Invalid model type"}), 400
+    except FileNotFoundError:
+        return jsonify({"error": f"Model '{model_type}' not found"}), 500
+
+    try:
+        features = np.array([[int(data['age']), int(data['gender']), int(data['impluse']), int(data['pressureHight']),
+                              int( data['pressureLow']), float(data['glucose']), float(data['kcm']), float(data['troponin'])]])
+        print(features)
+        if None in features:
+            raise ValueError("Some input features are None")
+    except KeyError as e:
+        return jsonify({"error": f"Missing feature: {str(e)}"}), 400
+    except ValueError as ve:
+        return jsonify({"error": str(ve)}), 400
+
+    try:
+        if model_type == 'mlp':
+            processed_input = preprocess_input(features)
+            prediction = model.predict(processed_input)[0]
+
+        else:
+            print(f"Model type: {type(model)}")
+            processed_input = preprocess_input(features)
+            prediction = model.predict(processed_input)[0]
+
+        # Calcular la probabilidad de infarto (clase positiva)
+        if hasattr(model, 'predict_proba'):
+            probability = model.predict_proba(features)[0][1] * 100  # Clase positiva es la segunda columna
+            print("Probability:", probability)
+        elif model_type == 'mlp':
+            if prediction > 0.5:
+                probability = prediction[0] * 100
+                prediction = 1
+            else:
+                probability = (prediction[0] * -100) + 100
+                prediction = 0
+        else:
+            probability = None
+            print("Probability is None")
+
+        print("Prediction:", prediction)
+        result = "Positive (Risk of Heart Attack)" if prediction == 1 else "Negative (No Risk of Heart Attack)"
+
+        response = {"prediction": result}
+        response["id"] = data['id']
+        if probability is not None:
+            response["percentage"] = f"{truncar_dos_decimales(probability):.2f}%"
+
+        print("Response:", response)
+
+        return jsonify(response)
+    except Exception as e:
+        return jsonify({"error": f"Prediction failed: {str(e)}"}), 500
+
 @app.route('/')
 def index():
     return "If you can see this message, backend is running! and you can start making predictions in the frontend."
